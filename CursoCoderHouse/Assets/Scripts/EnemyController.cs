ad3acfb4e277617f61179dfa0656bab23d663a7e
dfmr96 <dfmr96@gmail.com>
2023-02-08 00:13:40 -0300

    4th Homework
    Add :
    Enemy
    Enemy Rotation
    Enemy Movement
    Enemy States

---
+++ CursoCoderHouse/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public enum EnemyState
+{
+    Looking, Moving
+}
+public class EnemyController : MonoBehaviour
+{
+    [SerializeField] EnemyState state;
+    [SerializeField] GameObject player;
+    [SerializeField] float speed;
+    [SerializeField] float angularSpeed;
+    [SerializeField] float maxDistance;
+    private void Update()
+    {
+        switch (state)
+        {
+            case EnemyState.Looking:
+                RotateEnemy();
+                break;
+            case EnemyState.Moving:
+                RotateEnemy();
+                if (maxDistance > CheckDistance())
+                {
+                    MoveEnemy();
+                }
+                break;
+        }
+
+    }
+
+    private void RotateEnemy()
+    {
+        Quaternion rot = Quaternion.LookRotation((player.transform.position - transform.position));
+        transform.rotation = Quaternion.Lerp(transform.rotation, rot, angularSpeed * Time.deltaTime);
+    }
+
+    private void MoveEnemy()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, player.transform.position, speed * Time.deltaTime);
+    }
+
+    private float CheckDistance()
+    {
+        float distance = (player.transform.position - transform.position).magnitude;
+        return distance;
+    }
+}
