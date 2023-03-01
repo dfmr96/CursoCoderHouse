341bd964d0be90565dc3bad91f89df36f1af37bc
David Marcano <dfmr96@gmail.com>
2023-03-01 17:34:46 -0300

    Player can enter/exit rooms

---
+++ CursoCoderHouse/Assets/Scripts/Door.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    [SerializeField] GameObject player;
+    [SerializeField] GameObject roomMovePoint1;
+    [SerializeField] GameObject roomMovePoint2;
+    [SerializeField] Collider roomCollider1;
+    [SerializeField] Collider roomCollider2;
+
+    public void EnterToRoom(Collider col)
+    {
+        if (col ==roomCollider1)
+        {
+            player.transform.position = roomMovePoint1.transform.position;
+        }
+        if (col == roomCollider2)
+        {
+            player.transform.position = roomMovePoint2.transform.position;
+        }
+    }
+}
