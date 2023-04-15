09f61fc99ced0c2b06adce66f033ae281384de78
David Marcano <dfmr96@gmail.com>
2023-04-14 22:27:31 -0300

    Note View added

--- CursoCoderHouse/Assets/ScriptableObjects/ItemDataTypes/NoteItemData.cs
+++ CursoCoderHouse/Assets/ScriptableObjects/ItemDataTypes/NoteItemData.cs
@@ -1,11 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 [CreateAssetMenu(fileName = "New Note Item Data", menuName = "Inventory/New Note Item Data")]
 
 public class NoteItemData : ItemData
 {
+    public string Header => _header;
+    public string[] Pages => _pages;
+    public Sprite Background => _background;
+
+    [SerializeField] private string _header;
+    [SerializeField] private string[] _pages;
+    [SerializeField] private Sprite _background;
     private void Awake()
     {
         base._itemType = ItemType.Note;
--- CursoCoderHouse/Assets/Scripts/Note.cs
+++ CursoCoderHouse/Assets/Scripts/Note.cs
@@ -1,13 +1,26 @@
-using System.Collections;
-using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
+[RequireComponent(typeof(BoxCollider))]
 public class Note : MonoBehaviour, IInteractable
 {
-    [SerializeField] TextTrigger trigger;
-    [SerializeField] TextGenerator generator;
+    [SerializeField] private string _name;
+    [SerializeField] private string _description;
+    [SerializeField] private GameObject _noteViewer;
+    [SerializeField] private TMP_Text _header;
+    [SerializeField] private TMP_Text _pageField;
+    [SerializeField] private Button _leftBtn;
+    [SerializeField] private Button _rightBtn;
+    [SerializeField] private Image _background;
+    [SerializeField] private NoteItemData _itemData;
+
     public void Interact(ItemData item)
     {
-        generator.ShowMsg(trigger.text, 2f);
+        Time.timeScale = 0f;
+        _noteViewer.SetActive(true);
+        _header.SetText(_itemData.Header);
+        _pageField.SetText(_itemData.Pages[0]);
+        _background.sprite = _itemData.Background;
     }
 }
